037eb7b9913981cfd3fcb67e8f6fe03036e569c4
Violetea <Violetacolors@gmail.com>
2023-06-01 01:47:09 +0200

    Vistas categorías, menu según los roles.

---
+++ MiHadaMadrinaShop/Areas/Admin/Models/RoleDetailsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MiHadaMadrinaShop.Areas.Admin.Models
+{
+    public class RoleDetailsViewModel
+    {
+        public object Role { get; set; }
+        public object Users { get; set; }
+    }
+}
---
+++ MiHadaMadrinaShop/Areas/Admin/Models/RoleEditViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MiHadaMadrinaShop.Areas.Admin.Models
+{
+    public class RoleEditViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
